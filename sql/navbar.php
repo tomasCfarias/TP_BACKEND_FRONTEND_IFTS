18afb3df23a1b8b1f0f605c65141f860970d1cdc
AbrzzIan <133811113+AbrzzIan@users.noreply.github.com>
2023-09-28 10:02:49 -0300

    dropdown

--- sql/navbar.php
+++ sql/navbar.php
@@ -1,16 +1,25 @@
+<script src="js/navbar.js"></script>
 <header>
-        <nav>
-            <ul>
+        <nav class="navbar">
+            <div class="nav-item">
                 <li><a href="index.php"><b>INICIO</b></a></li>
                 <li><a href="login.php">Login</a></li>
                 <li><a href="signin.php">Sign In</a></li>
-            </ul>
-            <ul>
-                <li><a href="mostrarUsuarios.php">Lista Usuarios</a></li>
-                <li><a href="borrarUsuarios.php">Borrar Usuario</a></li>
-                <li><a href="modificarUsuarios.php">Modificar Usuario</a></li>
-                <li><a href="mostrarProductos.php">Lista Productos</a></li>
-                <li><a href="newproduct.php">Nuevo Articulo </a></li>
-            </ul>
+            </div>
+            <div class="nav-item">
+                <li class = "usuarios">Usuarios
+                    <ul class="dropdown-menu users">
+                        <li><a href="mostrarUsuarios.php">Lista Usuarios</a></li>
+                        <li><a href="borrarUsuarios.php">Borrar Usuario</a></li>
+                        <li><a href="modificarUsuarios.php">Modificar Usuario</a></li>
+                    </ul>
+                </li>
+                <li class = "productos">Productos
+                    <ul class="dropdown-menu products">
+                        <li><a href="mostrarProductos.php">Lista Articulos</a></li>
+                        <li><a href="newproduct.php">Nuevo Articulo </a></li>
+                    </ul>
+                </li>
+            </div>
         </nav>
 </header>
