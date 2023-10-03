02a95569d6be054714a3a15047b228e28c88a909
AbrzzIan <133811113+AbrzzIan@users.noreply.github.com>
2023-10-03 15:17:37 -0300

    arreglo bug con carrito vacio

--- api/navbartiendatailwind.php
+++ api/navbartiendatailwind.php
@@ -42,7 +42,7 @@
             <svg class="z-5 w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0h8m-8 0-1-4m9 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-9-4h10l2-7H3m2 7L3 4m0 0-.792-3H1"/>
             </svg>
-            <div class=" text-white text-xs relative bg-red-500 top-2 right-7 px-1 rounded-full"><?php echo count($_SESSION["cart_list"]) ?></div>
+            <div class=" text-white text-xs relative bg-red-500 top-2 right-7 px-1 rounded-full"><?php echo (!empty($_SESSION["cart_list"]) ? count($_SESSION["cart_list"]) : 0) ?></div>
   </button>
             <!-- Dropdown menu -->
             <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
