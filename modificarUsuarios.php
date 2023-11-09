ad227ee43cddd620b7f5ccf8760d87f6c83d5c90
AbrzzIan <133811113+AbrzzIan@users.noreply.github.com>
2023-11-09 14:27:31 -0300

    Arregla modificarUsuarios

--- modificarUsuarios.php
+++ modificarUsuarios.php
@@ -8,41 +8,11 @@ if (!isset($_SESSION['login_user'])) {
     exit(); 
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $numero = $_POST["id"];
-    $email = $_POST["email"];
-    $usuario = $_POST["usuario"];
-    
-    $password = $_POST["password"];
-    $hash = password_hash($password, PASSWORD_DEFAULT );
-
-    // Modificamos la consulta para usar una consulta preparada
-    $sql = "UPDATE usuarios SET email = ?, username = ?, password = ? WHERE id = ?";
-   
-    // Preparamos la consulta
-    $stmt = $conn->prepare($sql);
-
-    // Vinculamos los valores a la consulta
-    $stmt->bind_param("sssi", $email, $usuario, $hash, $numero);
-
-    // Ejecutamos la consulta
-    if ($stmt->execute()) {
-        header("Location: index.php");
-        exit();
-    } else {
-        echo "Error en la actualización: " . $stmt->error;
-    }
-
-    $stmt->close(); // Cerramos la consulta preparada
-}
-
-
 $numero = $_GET["id"];
 $sql = "SELECT * FROM usuarios WHERE id = '$numero'";
 $result = $conn->query($sql);
 $row = mysqli_fetch_array($result);
 
-$conn->close();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -59,18 +29,72 @@ $conn->close();
     <div class="main-container">
 
         <?php include_once("./api/sidebar.php") ?>
-        <form action="modificarUsuarios.php" method="POST" id="user_form">
+        <form action="modificarUsuarios.php?id=<?=$numero?>" method="POST" id="user_form">
+            <?php 
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+                $numero = $_POST["id"];
+                $email = $_POST["email"];
+                $usuario = $_POST["usuario"];
+                $password = $_POST["password"];
+                $hash = password_hash($password, PASSWORD_DEFAULT );
+            
+                //Check if email already in db
+                $sql = "SELECT * from usuarios WHERE email= '$email'";
+                $result = $conn -> query($sql);
+                $row2 = mysqli_fetch_array($result);
+                if (is_array($row2) && $row2["id"] != $numero) {
+                        ?>
+                      <div class="error" role="alert">
+                        <strong>Error!</strong>
+                        <p>El email ya esta registrado.</p>
+                      </div>  
+                      <?php
+                    }
+                elseif (!filter_var($email, FILTER_VALIDATE_EMAIL) || preg_match("/[^a-zA-Z0-9]+/",$usuario)) {
+            
+                    ?>
+                    <div class="error" role="alert">
+                      <strong>Error!</strong>
+                      <p>Formato de email o nombre invalido.</p>
+                    </div>  
+                    <?php
+                }
+                else {
+            
+                // Modificamos la consulta para usar una consulta preparada
+                $sql = "UPDATE usuarios SET email = ?, username = ?, password = ? WHERE id = ?";
+               
+                // Preparamos la consulta
+                $stmt = $conn->prepare($sql);
+            
+                // Vinculamos los valores a la consulta
+                $stmt->bind_param("sssi", $email, $usuario, $hash, $numero);
+            
+                // Ejecutamos la consulta
+                if ($stmt->execute()) {
+                    header("Location: index.php");
+                    exit();
+                } else {
+                    echo "Error en la actualización: " . $stmt->error;
+                }
+            
+                $stmt->close(); // Cerramos la consulta preparada
+                }
+            }
+            ?>
             <h2>Modificación de Usuario:</h2>
             <input type="hidden" name="id" value="<?= $row['id'] ?>"><br>
             
             <label for="email">email</label>
-            <input type="email" name="email" id="email" value="<?= $row['email'] ?>"><br>
+            <input type="email" name="email" id="email" value="<?= $row['email'] ?>" required><br>
             
             <label for="usuario">usuario</label>
-            <input type="text" name="usuario" id="usuario" value="<?= $row['username'] ?>"><br>
+            <input type="text" name="usuario" id="usuario" value="<?= $row['username'] ?>"r equired><br>
             
             <label for="password">contraseña</label>
-            <input type="password" name="password" id="password">
+            <input type="password" name="password" id="password" required>
             
             <input type="submit" id="enviar" value="Modificar">
         </form>
