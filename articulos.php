8873148013e65857bd874edaea89a49a4422841b
AbrzzIan <133811113+AbrzzIan@users.noreply.github.com>
2024-04-19 23:34:30 -0300

    cambio checkbox a radio

--- articulos.php
+++ articulos.php
@@ -55,9 +55,9 @@
         <b>Filtros</b>
             <form id = "filter_form">
                 <ul>
-                    <li><input type="checkbox" class="mr-1" name="categoria" value="1">Remeras</li>
-                    <li><input type="checkbox" class="mr-1" name="categoria" value="2">Pantalones</li>
-                    <li><input type="checkbox" class="mr-1" name="categoria" value="3">Camperas</li>
+                    <li><input type="radio" class="mr-1" name="categoria" value="1">Remeras</li>
+                    <li><input type="radio" class="mr-1" name="categoria" value="2">Pantalones</li>
+                    <li><input type="radio" class="mr-1" name="categoria" value="3">Camperas</li>
                     <button id="form_btn" class="text-white bg-blue-700 hover:cursor-pointer hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg my-2 text-sm px-5 py-1 text-center" value="Filtrar">Filtrar</button>
                 </ul>
             </form>
