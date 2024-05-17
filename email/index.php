67a3596d1e89f3dcac0b15aac79dd4fca294b24a
GERMAN <118678045+geerzio@users.noreply.github.com>
2024-05-17 00:13:03 -0300

    Update index.php
    se borra la contraseña del pasword del email de german

--- email/index.php
+++ email/index.php
@@ -31,7 +31,7 @@
         $mail -> Host = 'smtp.gmail.com';
         $mail -> SMTPAuth = true;
         $mail -> Username = 'germanluisponzio@gmail.com';
-        $mail -> Password = 'uuqf fxke qseg xyog';
+        $mail -> Password = '';
         $mail -> SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
